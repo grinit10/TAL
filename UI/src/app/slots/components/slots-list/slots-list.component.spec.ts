c52f936c96062bc8467545bc852260c22f153e95
Arnab Chakraborty <achakraborty@ranzcog.edu.au>
2021-04-21 23:54:38 +1000

    Adding angular UT.

--- UI/src/app/slots/components/slots-list/slots-list.component.spec.ts
+++ UI/src/app/slots/components/slots-list/slots-list.component.spec.ts
@@ -1,16 +1,34 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgxsModule, Store } from '@ngxs/store';
+import { TimeSlotState } from 'src/app/store/timeSlots/state/timeSlot.state';
 
 import { SlotsListComponent } from './slots-list.component';
 
 describe('SlotsListComponent', () => {
   let component: SlotsListComponent;
   let fixture: ComponentFixture<SlotsListComponent>;
+  let store: Store;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SlotsListComponent ]
+      imports: [
+        RouterTestingModule,
+        HttpClientModule,
+        NgxsModule.forRoot([TimeSlotState]),
+      ],
+      declarations: [SlotsListComponent]
     })
-    .compileComponents();
+      .compileComponents();
+    store = TestBed.inject(Store);
+    store.reset({
+      ...store.snapshot(),
+      timeSlot: {
+        items: [],
+        isLoading: false
+      }
+    });
   });
 
   beforeEach(() => {
@@ -21,5 +39,18 @@ describe('SlotsListComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should update store on init', () => {
+    component.ngOnInit();
+    expect(store.selectSnapshot(state => state.timeSlot.isLoading)).toEqual(true);
+  });
+
+  it('should run isSlotAvailable successfully', () => {
+    expect(component.isSlotAvailable({
+      startTime: new Date(),
+      startTimeDisplay: '09:00 AM',
+      isAvailable: true
+    })).toEqual(true);
   });
 });
